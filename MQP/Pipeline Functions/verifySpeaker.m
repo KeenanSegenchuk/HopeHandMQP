b0c8066ccb9f86fa55a5c2bd9d3b32f81f91c35b
KeenanSegenchuk <71797879+KeenanSegenchuk@users.noreply.github.com>
2024-02-29 17:32:18 -0500

    Add files via upload

--- MQP/Pipeline Functions/verifySpeaker.m
+++ MQP/Pipeline Functions/verifySpeaker.m
@@ -1,12 +1,12 @@
 function user = verifySpeaker(audio, ivs)
 %   return true if the audio is from a user enrolled in
-    disp("1")
+
     %certainty threshold for accepting audio as being from user 
-    threshold = .177;
+    threshold = 1 - .177;
 
     results = identify(ivs,audio,"plda");
-
-    if results.Score(1) < threshold
+    disp(results.Score(1))
+    if results.Score(1) > threshold
         user = true;
     else
         user = false;
